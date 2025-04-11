cb9991172f6f2b855ee45bf85bd2a76554ecc904
DeibidSE <davidsimon98@outlook.com>
2025-04-11 19:14:56 +0200

    Change BadgeComponent color prop to accept any string and update color mapping

--- components/BadgeComponent.vue
+++ components/BadgeComponent.vue
@@ -22,7 +22,7 @@
 <script setup lang="ts">
 const props = defineProps<{
 	tag: keyof HTMLElementTagNameMap
-	color?: 'blue' | 'red' | 'green' | 'yellow' | 'purple' | 'orange' | 'pink' | 'accent' | 'accent-secondary'
+	color?: string
 	prependIcon?: string
 	appendIcon?: string
 	hover?: boolean
@@ -30,16 +30,14 @@ const props = defineProps<{
 }>()
 
 const colorsMap = {
-	'blue': 'bg-blue-100 text-blue-900 dark:bg-blue-800/30 dark:text-blue-100 border-blue-400/30',
-	'red': 'bg-red-100 text-red-900 dark:bg-red-800/30 dark:text-red-300 border-red-400/30',
-	'green': 'bg-green-100 text-green-900 dark:bg-green-800/30 dark:text-green-300 border-green-400/30',
-	'yellow': 'bg-yellow-100 text-yellow-900 dark:bg-yellow-700/30 dark:text-yellow-300 border-yellow-400/30',
-	'purple': 'bg-purple-100 text-purple-900 dark:bg-purple-800/30 dark:text-purple-300 border-purple-400/30',
-	'orange': 'bg-orange-100 text-orange-900 dark:bg-orange-800/30 dark:text-orange-300 border-orange-400/30',
-	'pink': 'bg-pink-100 text-pink-900 dark:bg-pink-800/30 dark:text-pink-300 border-pink-400/30',
-	'accent': 'bg-[#f2e6fc] text-[#7927b2] dark:bg-[#7927b2]/10 dark:text-[#c79cf1] border-[#7927b2]/40',
-	'accent-secondary': 'bg-[#ffe5f2] text-[#FB3182] dark:bg-[#FB3182]/10 dark:text-[#ff88ba] border-[#FB3182]/40',
-	'default': 'bg-primary-light text-dark dark:bg-secondary-dark dark:text-light border-gray-400/20',
+	blue: 'bg-blue-100 dark:bg-blue-800/30 text-blue-900 dark:text-blue-100 border-blue-400/30',
+	red: 'bg-red-100 dark:bg-red-800/30 text-red-900 dark:text-red-300 border-red-400/30',
+	green: 'bg-green-100 dark:bg-green-800/30 text-green-900 dark:text-green-300 border-green-400/30',
+	yellow: 'bg-yellow-100 dark:bg-yellow-700/30 text-yellow-900 dark:text-yellow-300 border-yellow-400/30',
+	purple: 'bg-purple-100 dark:bg-purple-800/30 text-purple-900 dark:text-purple-300 border-purple-400/30',
+	orange: 'bg-orange-100 dark:bg-orange-800/30 text-orange-900 dark:text-orange-300 border-orange-400/30',
+	pink: 'bg-pink-100 dark:bg-pink-800/30 text-pink-900 dark:text-pink-300 border-pink-400/30',
+	default: 'bg-primary-light dark:bg-secondary-dark text-dark dark:text-light border-dark/30 dark:border-light/40',
 }
 
 const sizeMap = {
@@ -49,5 +47,5 @@ const sizeMap = {
 }
 
 const sizeClasses = computed(() => sizeMap[props.size ?? 'medium'])
-const colorClasses = computed(() => colorsMap[props.color ?? 'default'])
+const colorClasses = computed(() => colorsMap[props.color as 'blue' | 'red' | 'green' | 'yellow' | 'purple' | 'orange' | 'pink' ?? 'default'])
 </script>
