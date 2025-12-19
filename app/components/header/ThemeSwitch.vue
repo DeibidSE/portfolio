fbd262ecd48088dfce39d53b5bc265ac72438939
DeibidSE <davidsimon98@outlook.com>
2025-12-19 19:45:11 +0100

    Add transition to switch between dark and light modes

--- app/components/header/ThemeSwitch.vue
+++ app/components/header/ThemeSwitch.vue
@@ -1,7 +1,7 @@
 <template>
 	<div
 		class="flex items-center justify-center p-2 rounded-full cursor-pointer hover:scale-110 hover:text-yellow-500 dark:hover:text-accent"
-		@click="setTheme"
+		@click="toggleTheme"
 	>
 		<div
 			class="transition-transform duration-500"
@@ -30,7 +30,38 @@ const setTheme = () => {
 	themeStore.setTheme(!themeStore.isLightMode ? 'light' : 'dark')
 }
 
+const toggleTheme = () => {
+	if (!document.startViewTransition) setTheme()
+	document.startViewTransition(setTheme)
+}
+
 onMounted(() => {
 	themeStore.initTheme()
 })
 </script>
+
+<style lang="css">
+::view-transition-new(root) {
+  mask: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><circle cx="20" cy="20" r="20" fill="white"/></svg>')
+    center / 0 no-repeat;
+  animation: scale 1s;
+  animation-fill-mode: both;
+}
+
+::view-transition-old(root),
+.dark::view-transition-old(root) {
+  animation: none;
+  animation-fill-mode: both;
+  z-index: -1;
+}
+.dark::view-transition-new(root) {
+  animation: scale 1s;
+  animation-fill-mode: both;
+}
+
+@keyframes scale {
+  to {
+    mask-size: 200vmax;
+  }
+}
+</style>
